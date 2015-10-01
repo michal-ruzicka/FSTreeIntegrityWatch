83c946af10d0d6e748c245bfa66a993cf110b7da
Lukáš Hejtmánek <xhejtman@ics.muni.cz>
2015-10-01 17:19:05 +0200

    Add the ability to use multiple hashes at once and the silent mode.

--- extattr-file-integrity.c
+++ extattr-file-integrity.c
@@ -35,31 +35,62 @@ int follow_links_opt=0;
 
 int initqdepth=0;
 
-void walk_down_dir(char *directory, GQueue *worknames);
+int quiet=0;
+
+int *digests;
+int n_digests = sizeof digest_types / sizeof(struct digest_t);
+
 int is_dir(char *name);
 int walk_dir(GQueue *worknames, int recursive);
 int process_file(char *name);
-void walk_down_dir(char *directory, GQueue *worknames);
-
-char attr_name[100];
+int walk_down_dir(char *directory, GQueue *worknames);
 
 int
-main(int argc, char *argv[]) 
+main(int argc, char *argv[])
 {
         GQueue *worknames=g_queue_new();
         char recursive=0;
         int opt;
-        char *dig="SHA512";
+        int dig=0;
         int i;
         gnutls_hash_hd_t digest;
 
-        while((opt = getopt(argc, argv, "rd:svlh")) != -1) {
+        digests = calloc(n_digests+1, sizeof(int));
+
+        while((opt = getopt(argc, argv, "rd:svlhq")) != -1) {
                 switch(opt) {
                         case 'r':
                                 recursive = 1;
                                 break;
                         case 'd':
-                                dig = optarg;
+                                if(strcmp(optarg, "help") == 0) {
+                                        fprintf(stderr, "Available digests:");
+                                        for(i=0; digest_types[i].name != NULL; i++) {
+                                                fprintf(stderr, " %s", digest_types[i].name);
+                                        }
+                                        fprintf(stderr, "\n");
+                                        return 0;
+                                }
+                                for(i=0; digest_types[i].name != NULL; i++) {
+                                        if(strcmp(digest_types[i].name, optarg) == 0) {
+                                                digests[dig++] = i;
+                                                if(dig >= n_digests) {
+                                                        fprintf(stderr, "Too many digests requested.\n");
+                                                        return 1;
+                                                }
+                                                if(gnutls_hash_init(&digest, digest_types[i].type)!=0) {
+                                                        fprintf(stderr,
+                                                                  "Cannot init gnutls hash functions\n");
+                                                        return 1;
+                                                }
+                                                gnutls_hash_deinit(digest, NULL);
+                                                break;
+                                        }
+                                }
+                                if(digest_types[i].name == NULL) {
+                                        fprintf(stderr, "Unknown digest type %s\n", optarg);
+                                        return 1;
+                                }
                                 break;
                         case 'v':
                                 verify_opt = 1;
@@ -70,41 +101,27 @@ main(int argc, char *argv[])
                         case 'l':
                                 follow_links_opt = 1;
                                 break;
+                        case 'q':
+                                quiet=1;
+                                break;
                         case 'h':
                         default:
-                                fprintf(stderr, "Usage: hasher [-r] [-d digest] [-s] [-v] [-h] [-l] files\n");
-                                fprintf(stderr, "\t-r recursive\n\t-d digest (type -d help)\n\t-s set digest\n\t-v verify digest\n\t-h print this help\n\t-l follow symlinks\n");
+                                fprintf(stderr, "Usage: hasher [-r] [-d digest [-d digest ...]] [-s] [-v] [-h] [-l] [-q] files\n");
+                                fprintf(stderr, "\t-r recursive\n\t-d digest (type -d help)\n\t-s set digest\n\t-v verify digest\n\t-h print this help\n\t-l follow symlinks\n\t-q quiet\n");
                                 exit(1);
                 }
         }
+
+        n_digests=dig;
+        digests[dig] = -1;
 
         if(set_opt == 1 && verify_opt == 1) {
                 fprintf(stderr, "set and verify options are mutually exclusive\n");
                 return 1;
         }
 
-        if(set_opt == 0 && verify_opt == 0) {
+        if(set_opt == 0 && verify_opt == 0 && !quiet) {
                 fprintf(stderr, "set nor verify specified, doing dry run\n");
-        }
-
-        for(i=0; digest_types[i].name != NULL; i++) {
-                if(strcmp(digest_types[i].name, dig) == 0) {
-                        dig_alg = digest_types[i].type;
-                        snprintf(attr_name, 100, "user.extattr-file-integrity.%s", digest_types[i].name);
-                        break;
-                }
-        }
-
-        if(dig_alg == GNUTLS_DIG_UNKNOWN) {
-                if(strcmp(dig, "help")!=0) {
-                        fprintf(stderr, "Unknown digest type %s\n", dig);
-                }
-                fprintf(stderr, "Available digests:");
-                for(i=0; digest_types[i].name != NULL; i++) {
-                        fprintf(stderr, " %s", digest_types[i].name);
-                }
-                fprintf(stderr, "\n");
-                return 1;
         }
 
         if(optind == argc) {
@@ -116,18 +133,11 @@ main(int argc, char *argv[])
                 initqdepth++;
         }
 
-        if(gnutls_hash_init(&digest, dig_alg)!=0) {
-                fprintf(stderr, "Cannot init gnutls hash functions\n");
-                return 1;
-        }
-
-        gnutls_hash_deinit(digest, NULL);
-
         return walk_dir(worknames, recursive);
 }
 
 int
-is_dir(char *name) 
+is_dir(char *name)
 {
         struct stat sb;
         int ret;
@@ -138,7 +148,8 @@ is_dir(char *name)
                 ret = lstat(name, &sb);
         }
         if(ret != 0) {
-                fprintf(stderr, "error stating file %s ", name);
+                if(!quiet)
+                        fprintf(stderr, "error stating file %s ", name);
                 perror(NULL);
                 return -1;
         }
@@ -146,38 +157,48 @@ is_dir(char *name)
 }
 
 int
-walk_dir(GQueue *worknames, int recursive) 
+walk_dir(GQueue *worknames, int recursive)
 {
         char *item;
         int ret;
+        int err_code=0;
 
         while((item=g_queue_pop_head(worknames))!=NULL) {
                 if(initqdepth >= 0)
                         initqdepth--;
                 ret = is_dir(item);
-                if(ret == -1) {
+                if(ret == -1 && !quiet) {
                         fprintf(stderr, "Cannot stat %s\n", item);
+                        err_code = -1;
                 }
                 if(ret == 0) {
-                        process_file(item);
+                        if(process_file(item)!=0)
+                                err_code = -1;
                 }
                 if(ret == 1) {
                         if(recursive || initqdepth >= 0) {
-                                fprintf(stderr, "Walking down %s\n", item);
-                                walk_down_dir(item, worknames);
+                                if(!quiet)
+                                        fprintf(stderr, "Walking down %s\n", item);
+                                if(walk_down_dir(item, worknames)!=0)
+                                        err_code=-1;
                         } else {
-                                fprintf(stderr, "%s is directory, skipped in non-recursive mode.\n", item);
+                                if(!quiet)
+                                        fprintf(stderr,
+                                                "%s is directory, skipped in non-recursive mode.\n", item);
                         }
                 }
                 free(item);
         }
-        return 0;
+        return err_code;
 }
 
-void
-set_hash(const char *name, const char *hash, int size)
+int
+set_hash(const char *name, const char *hash, int size, int dig)
 {
         int ret;
+        char attr_name[100];
+
+        snprintf(attr_name, 100, "user.extattr-file-integrity.%s", digest_types[dig].name);
 
         if(follow_links_opt) {
                 ret = setxattr(name, attr_name, hash, size, 0);
@@ -185,17 +206,23 @@ set_hash(const char *name, const char *hash, int size)
                 ret = lsetxattr(name, attr_name, hash, size, 0);
         }
 
-        if(ret != 0) {
+        if(ret != 0 && !quiet) {
                 fprintf(stderr, "Cannot set xattr on %s ", name);
                 perror("");
+                return -1;
         }
+        return 0;
 }
 
-void
-verify_hash(const char *name, const char *hash, int size)
+int
+verify_hash(const char *name, const char *hash, int size, int dig)
 {
         char buff[size];
         int ret;
+
+        char attr_name[100];
+
+        snprintf(attr_name, 100, "user.extattr-file-integrity.%s", digest_types[dig].name);
 
         if(follow_links_opt) {
                 ret = getxattr(name, attr_name, buff, size);
@@ -203,43 +230,53 @@ verify_hash(const char *name, const char *hash, int size)
                 ret = lgetxattr(name, attr_name, buff, size);
         }
 
-        if(ret < 1) {
+        if(ret < 1 && !quiet) {
                 fprintf(stderr, "Cannot get xattr on %s ", name);
                 perror("");
-                return;
+                return -1;
         }
 
         if(memcmp(buff, hash, size) != 0) {
-                fprintf(stderr, "Checksum ERROR on %s\n", name);
+                if(!quiet)
+                        fprintf(stderr, "Checksum ERROR on %s\n", name);
+                return -1;
         } else {
-                fprintf(stderr, "Checksum OK on %s\n", name);
+                if(!quiet)
+                        fprintf(stderr, "Checksum OK on %s\n", name);
         }
+        return 0;
 }
 
 int
-process_file(char *name) 
+process_file(char *name)
 {
         FILE *in;
         char buff[16384];
         int usable=0;
         int ret;
-        gnutls_hash_hd_t digest;
-        int hash_len;
-        unsigned char *hash;
-        int i;
+        gnutls_hash_hd_t digest[n_digests];
+        int hash_len[n_digests];
+        unsigned char *hash[n_digests];
+        int i,j;
+        int err_code=0;
 
-        gnutls_hash_init(&digest, dig_alg);
+        for(i=0; i < n_digests; i++) {
+                gnutls_hash_init(&digest[i], digest_types[digests[i]].type);
 
-        hash_len = gnutls_hash_get_len(dig_alg);
+                hash_len[i] = gnutls_hash_get_len(digest_types[digests[i]].type);
 
-        hash = calloc(1, hash_len);
+                hash[i] = calloc(1, hash_len[i]);
+        }
 
-        fprintf(stderr, "Processing %s...\n", name);
+        if(!quiet)
+                fprintf(stderr, "Processing %s...\n", name);
         in = fopen(name, "rb");
 
         if(!in) {
-                fprintf(stderr, "Cannot open file ");
-                perror(name);
+                if(!quiet) {
+                        fprintf(stderr, "Cannot open file ");
+                        perror(name);
+                }
                 return -1;
         }
 
@@ -249,7 +286,8 @@ process_file(char *name)
                         break;
                 }
                 usable = 1;
-                gnutls_hash(digest, buff, ret);
+                for(i=0; i < n_digests; i++)
+                        gnutls_hash(digest[i], buff, ret);
         }
 
         if(usable == 0) {
@@ -258,31 +296,37 @@ process_file(char *name)
 
         fclose(in);
 
-        gnutls_hash_deinit(digest, hash);
+        for(i=0; i < n_digests; i++) {
+                gnutls_hash_deinit(digest[i], hash[i]);
 
-        fprintf(stderr, "Hash: ");
+                if(!quiet) {
+                        fprintf(stderr, "Hash %s: ", digest_types[digests[i]].name);
 
-        for(i=0; i < hash_len; i++) {
-                fprintf(stderr, "%x", hash[i]);
+                        for(j=0; j < hash_len[i]; j++) {
+                                fprintf(stderr, "%x", hash[i][j]);
+                        }
+
+                        fprintf(stderr, "\n");
+                }
+
+                if(set_opt) {
+                        if(set_hash(name, (const char*)hash[i], hash_len[i], digests[i])!=0)
+                                err_code=-1;
+                }
+
+                if(verify_opt) {
+                        if(verify_hash(name, (const char*)hash[i], hash_len[i], digests[i])!=0)
+                                err_code=-1;
+                }
+
+                free(hash[i]);
         }
 
-        fprintf(stderr, "\n");
-
-        if(set_opt) {
-                set_hash(name, (const char*)hash, hash_len);
-        }
-
-        if(verify_opt) {
-                verify_hash(name, (const char*)hash, hash_len);
-        }
-
-        free(hash);
-
-        return 0;
+        return err_code;
 }
 
-void
-walk_down_dir(char *directory, GQueue *worknames) 
+int
+walk_down_dir(char *directory, GQueue *worknames)
 {
         DIR *dir;
         struct dirent *de;
@@ -292,7 +336,7 @@ walk_down_dir(char *directory, GQueue *worknames)
 
         if(!dir) {
                 perror(directory);
-                return;
+                return -1;
         }
 
         while((de = readdir(dir))) {
@@ -301,9 +345,11 @@ walk_down_dir(char *directory, GQueue *worknames)
                 if(strcmp(de->d_name, "..") == 0)
                         continue;
                 snprintf(buff, PATH_MAX+NAME_MAX, "%s/%s", directory, de->d_name);
-                fprintf(stderr, "Adding %s\n", buff);
+                if(!quiet)
+                        fprintf(stderr, "Adding %s\n", buff);
                 g_queue_push_head(worknames, strdup(buff));
         }
 
         closedir(dir);
+        return 0;
 }
